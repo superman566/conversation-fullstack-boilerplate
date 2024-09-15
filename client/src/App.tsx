7c0ec666521c3014110e5f22692d2c3aa2c22e81
Guochao-He <guochao.he@ibm.com>
2024-09-15 15:17:47 -0400

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-primary">Hello daisyUI</button>
+    </>
+  )
+}
+
+export default App
